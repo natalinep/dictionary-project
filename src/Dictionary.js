aa60e3b15e7b71266a35b3254499dd555c434ec2
natalinep <nep.natali@gmail.com>
2022-08-30 17:26:10 +0300

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
